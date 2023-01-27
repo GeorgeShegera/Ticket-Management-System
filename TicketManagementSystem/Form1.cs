fc0fab5c5bae51132feda28e5be48b252d42c4ca
GeorgeShegera <113610588+GeorgeShegera@users.noreply.github.com>
2023-01-27 22:27:00 +0200

    Json

--- TicketManagementSystem/Form1.cs
+++ TicketManagementSystem/Form1.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ManagementSystemLibrary;
+using Newtonsoft.Json;
+using TicketManagementSystem;
 
 namespace TicketManagementSystem
-{
-    public partial class Form1 : Form
+{    
+    public partial class AuthorizationWnd : Form
     {
-        public Form1()
+        public List<User> Users { get; set; }
+        public AuthorizationWnd()
         {
+            string json = "";
+            using(FileStream stream = new FileStream("Users.json", FileMode.OpenOrCreate))
+            {
+                using(StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    while (!reader.EndOfStream) json += reader.ReadLine();
+                }
+            }
+            Users = JsonConvert.DeserializeObject<List<User>>(json);
             InitializeComponent();
         }
     }
